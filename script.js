9f42db911067fc03800ece5803a3e9571bcc4cf4
khoapham0112 <khoapham1122007@gmail.com>
2025-07-28 10:39:28 +0700

    .

---
+++ script.js
@@ -0,0 +1,27 @@
+let result = document.querySelector("#result");
+let allClearbtn = document.querySelector("#allClear");
+let clearbtn = document.querySelector("#clear");
+let modolubtn = document.querySelector("#modolu");
+let divisionbtn = document.querySelector("#division");
+let addbtn = document.querySelector("#add");
+let minusbtn = document.querySelector("#minus");
+let multiplybtn = document.querySelector("#multiply");
+let dotbtn = document.querySelector("#dot");
+let equalbtn = document.querySelector("#equal");
+let digit1btn = document.querySelector("#number1");
+let digit2btn = document.querySelector("#number2");
+let digit3btn = document.querySelector("#number3");
+let digit4btn = document.querySelector("#number4");
+let digit5btn = document.querySelector("#number5");
+let digit6btn = document.querySelector("#number6");
+let digit7btn = document.querySelector("#number7");
+let digit8btn = document.querySelector("#number8");
+let digit9btn = document.querySelector("#number9");
+
+
+
+
+
+
+
+
